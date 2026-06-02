85781cfcc194bb53c09a5071bf82bc20cfad1a15
Valery00781 <valery.vovan@gmail.com>
2026-06-02 15:16:41 +0200

    Update (extends Person) Paramedic.java

--- Paramedic.java
+++ Paramedic.java
@@ -1,27 +1,18 @@
-public class Paramedic {
-    private String id;
-    private String name;
-    private Coordinate currentPosition;
+public class Paramedic extends Person {
 
     public Paramedic(String id, String name, Coordinate currentPosition) {
-        this.id = id;
-        this.name = name;
-        this.currentPosition = currentPosition;
+        super(id, name, currentPosition);
     }
 
     public void followItinerary(Itinerary itinerary) {
         for (Coordinate point : itinerary.getWaypoints()) {
-            this.currentPosition = point;
-            System.out.println(name + " moving to (" + point.getLatitude() + ", " + point.getLongitude() + ")");
+            this.position = point;
+            System.out.println(name + " moving to " + point);
         }
     }
 
     public void arriveAtHospital(Hospital hospital) {
-        this.currentPosition = hospital.getPosition();
-        System.out.println(name + " arrived at hospital " + hospital.getName());
+        this.position = hospital.getPosition();
+        System.out.println(name + " arrived at " + hospital.getName());
     }
-
-    public String getId() { return id; }
-    public String getName() { return name; }
-    public Coordinate getCurrentPosition() { return currentPosition; }
 }
